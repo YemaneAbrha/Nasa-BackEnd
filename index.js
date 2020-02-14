2cce99978293f5cc60e58e3e55fb6184b44a27d7
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-14 13:16:29 +0300

    addig some functionality

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const express = require('express');
-const bodyparser = require('body-parser');
+require('dotenv').config()
+const express = require('express')
+const bodyparser = require('body-parser')
 
-const app = express();
+
+
+const app = express()
+
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+
+const port = process.env.PORT
+const env = process.env.NODE_ENV
+
+app.listen(port, () => { console.log(`Server Started at ${port}`) });
