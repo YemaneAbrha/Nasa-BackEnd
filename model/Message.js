9b81301f9e5e22b337bacc6d2129fdbbc5fb4c6f
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-23 11:47:14 +0300

    Finsh The model

--- model/Message.js
+++ model/Message.js
@@ -5,9 +5,39 @@ const MessageSchema = mongoose.Schema({
         type: String,
         require: true,
 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
-const Message = mongoose.model('Message', MessageSchema);
+class MessageClass {
+
+    static async getMessage() {
+        const message = await this.find()
+            .sort({ createdAt: -1 })
+            .skip(0)
+            .limit(10);
+        return message;
+    }
+
+    static async setMessage({ body }) {
+        return this.create({
+            body
+        });
+    }
+
+    static async editMessage({ id, body }) {
+        const filter = { _id: id };
+        const update = { body: body }
+        const editedMessage = this.findOneAndUpdate(filter, update, {
+            new: true
+        });
+        return editedMessage;
+    }
+}
+MessageSchema.loadClass(MessageClass)
+const Message = mongoose.model("Message", MessageSchema);
 module.exports = {
     Message
 }
