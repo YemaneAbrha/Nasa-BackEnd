428ff83de30e4793569ff2d216d71621922fba90
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-27 20:42:38 +0300

    Addd Mlab

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
-
-
+const db = process.env.MONGODB_URL;
+// mongodb + srv://yemane:yemane@newcluster-vevfg.mongodb.net/test?retryWrites=true&w=majority
 const app = express()
-
+// mongodb + srv://yemane:yemane@newcluster-vevfg.mongodb.net/test?retryWrites=true&w=majority
+// mongodb + srv://yemane:yemaneabrha@newcluster-vevfg.mongodb.net/test?retryWrites=true&w=majority
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use('/uploads', express.static('uploads'));
 const DB_URI = process.env.DB_URI
 //Database
 
-mongoose.connect(DB_URI, {
+mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
